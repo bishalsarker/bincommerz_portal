587c70c4dc84305eaab0a4115f08e210f37026d6
Bishal Sarker <mr.bishalsarker@gmail.com>
2021-09-05 02:19:17 +0600

    environment variable issue fix

--- projects/content-management/src/app/constants/api-constants.ts
+++ projects/content-management/src/app/constants/api-constants.ts
@@ -1,4 +1,4 @@
-import { environment } from "projects/dashboard/src/environments/environment";
+import { environment } from "../../environments/environment";
 
 export var API_HOST: string = 
     window.location.hostname.includes("localhost") ? 
